1c84679c39821d5323cf79ee6d4fd3183f7b1492
asepsyaepull <asep.syaepul95@gmail.com>
2024-12-28 12:59:24 +0700

    Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  base: '/Mini-Project-2-React/' // Tambahkan base path sesuai dengan nama repositori Anda
 })
